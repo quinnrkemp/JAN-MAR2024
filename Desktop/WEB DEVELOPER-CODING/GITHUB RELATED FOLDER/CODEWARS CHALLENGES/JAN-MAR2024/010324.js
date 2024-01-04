7d33a1f78a0cae2763b69af14a8af7ea08d31626
quinnrkemp <a467814@gmail.com>
2024-01-03 23:27:52 -0500

    coding chall for 010324

---
+++ Desktop/WEB DEVELOPER-CODING/GITHUB RELATED FOLDER/CODEWARS CHALLENGES/JAN-MAR2024/010324.js
@@ -0,0 +1,34 @@
+// CHALLENGE NAME-  Template Strings (#2)
+
+    // DESCRIPTION:
+
+    // Task
+    // Your task is to return the correct string using the Template 
+    // String Feature.
+    // Input
+    // Two Strings, no validation is needed.
+    // Output
+    // You must output a string containing the two strings with the 
+    // word ```' are '```
+    // Reference: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/template_strings
+
+    // // ***STARTER CODE***
+
+    // var TempleStrings = function(obj, feature) {
+    //     return //complete this
+    //   }  
+
+    // //   ******TEST CASES*****
+    
+    // describe("Tests", () => {
+    //     it("test", () => {
+    //   Test.assertEquals(TempleStrings("Animals","Good"), 'Animals are Good')
+      
+    //     });
+    //   });
+      
+    // ******MY ANSWER********
+
+    function templateStrings (noun, adjective) {
+        return `${noun} are ${adjective}`
+      }
