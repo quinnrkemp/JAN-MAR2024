c74b2d1875ad66eacfc09544f86a8c994a2bb468
quinnrkemp <a467814@gmail.com>
2024-02-26 17:42:50 -0500

    coding chall for 022624

---
+++ Desktop/WEB DEVELOPER-CODING/GITHUB RELATED FOLDER/CODEWARS CHALLENGES/JAN-MAR2024/022624.js
@@ -0,0 +1,35 @@
+// CHALLENGE NAME-  Volume of a Cuboid  (#2)
+
+// DESCRIPTION:
+
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. 
+// Write a function to help Bob with this calculation.
+
+// // ***STARTER CODE***
+
+// class Kata {
+//     static getVolumeOfCuboid(length, width, height) {
+//       // your code here
+//     }
+//   } 
+
+// //   ******TEST CASES*****
+
+// const { assert } = require('chai')
+
+// describe("Tests", () => {
+//   it("Example tests", () => {
+//     assert.strictEqual(Kata.getVolumeOfCuboid(1,2,2),  4);
+//     assert.strictEqual(Kata.getVolumeOfCuboid(6,2,5), 60);
+//   });
+// });
+ 
+
+// ******MY ANSWER********
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      // your code here
+      return length*width*height
+    }
+  }
