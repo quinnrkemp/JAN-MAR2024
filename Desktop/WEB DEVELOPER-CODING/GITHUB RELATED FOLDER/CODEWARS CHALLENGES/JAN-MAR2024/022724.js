9e8713037be21c3b3282d13d0f35740b78ef5470
quinnrkemp <a467814@gmail.com>
2024-02-27 21:14:49 -0500

    coding chall for 022724

---
+++ Desktop/WEB DEVELOPER-CODING/GITHUB RELATED FOLDER/CODEWARS CHALLENGES/JAN-MAR2024/022724.js
@@ -0,0 +1,30 @@
+// CHALLENGE NAME- You Cant Code Under Pressure#1   (#2)
+
+// DESCRIPTION:
+
+// Code as fast as you can! You need to double the integer and return it.
+
+// // ***STARTER CODE***
+
+// function doubleInteger(i) {
+//     // i will be an integer. Double it and return it.
+//     return i;
+//   } 
+
+// //   ******TEST CASES*****
+
+// const Test = require('@codewars/test-compat');
+
+// describe("Tests", () => {
+//   it("test", () => {
+// Test.assertEquals(doubleInteger(2), 4);
+//   });
+// });
+ 
+
+// ******MY ANSWER********
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i+i
+  }
