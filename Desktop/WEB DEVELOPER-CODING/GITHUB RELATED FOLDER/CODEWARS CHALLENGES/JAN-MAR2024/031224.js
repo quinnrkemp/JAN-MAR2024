ede1776e083320d1a379bb39aeed0b2050905207
quinnrkemp <a467814@gmail.com>
2024-03-12 22:02:16 -0400

    coding chall for 031224

---
+++ Desktop/WEB DEVELOPER-CODING/GITHUB RELATED FOLDER/CODEWARS CHALLENGES/JAN-MAR2024/031224.js
@@ -0,0 +1,42 @@
+// CHALLENGE NAME-  Exclamation marks series #2: Remove all   (#2)
+// exclamation marks from the end of sentence
+
+// DESCRIPTION:
+
+// Remove all exclamation marks from the end of sentence.
+
+// Examples
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi"
+// remove("!Hi") === "!Hi"
+// remove("!Hi!") === "!Hi"
+// remove("Hi! Hi!") === "Hi! Hi"
+// remove("Hi") === "Hi"
+
+// // ***STARTER CODE***
+
+// function remove (string) {  
+//     return '';
+//   }
+
+// //   ******TEST CASES*****
+
+// const strictEqual = require("chai").assert.strictEqual;
+
+// function doTest (input, expected) {
+// 	const actual = remove(input);
+// 	strictEqual(actual, expected, `for string:\n"${input}"\n`);
+// }
+
+// it("It should works for basic tests", function () {
+// 	doTest("Hi!", "Hi")
+// 	doTest("Hi!!!", "Hi")
+// 	doTest("!Hi", "!Hi")
+// 	doTest("!Hi!", "!Hi")
+// 	doTest("Hi! Hi!", "Hi! Hi")
+// 	doTest("Hi", "Hi")
+// });
+
+// ******MY ANSWER********
+
+// ******I could not find a correct answer to this one that I liked*****
