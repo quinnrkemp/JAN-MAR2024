56e0cce015e8f36cf4cd7a15c448380ca23c66b3
quinnrkemp <a467814@gmail.com>
2024-07-09 15:18:05 -0400

    coding chall for 070924

---
+++ Desktop/WEB DEVELOPER-CODING/GITHUB RELATED FOLDER/CODEWARS CHALLENGES/JAN-MAR2024/070924.js
@@ -0,0 +1,53 @@
+    // CHALLENGE NAME-  Exclamation marks series #6: Remove n exclamation 
+    //                  marks in the sentence from left to right
+
+    // DESCRIPTION:
+
+    // Remove n exclamation marks in the sentence from left to right. n is 
+    // positive integer.
+
+    // Examples
+    // remove("Hi!",1) === "Hi"
+    // remove("Hi!",100) === "Hi"
+    // remove("Hi!!!",1) === "Hi!!"
+    // remove("Hi!!!",100) === "Hi"
+    // remove("!Hi",1) === "Hi"
+    // remove("!Hi!",1) === "Hi!"
+    // remove("!Hi!",100) === "Hi"
+    // remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+    // remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+    // remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+    // remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+
+    // // ***STARTER CODE***
+
+    // function remove(s,n){
+    //     //coding and coding....
+            
+    //   }
+
+    // //   ******TEST CASES*****
+    
+    // const Test = require('@codewars/test-compat');
+
+    // describe("Basic Tests", function(){
+    //   it("It should works for basic tests", function(){
+    
+    // Test.assertSimilar(remove("Hi!",1) , "Hi")
+    // Test.assertSimilar(remove("Hi!",100) , "Hi")
+    // Test.assertSimilar(remove("Hi!!!",1) , "Hi!!")
+    // Test.assertSimilar(remove("Hi!!!",100) , "Hi")
+    // Test.assertSimilar(remove("!Hi",1) , "Hi")
+    // Test.assertSimilar(remove("!Hi!",1) , "Hi!")
+    // Test.assertSimilar(remove("!Hi!",100) , "Hi")
+    // Test.assertSimilar(remove("!!!Hi !!hi!!! !hi",1) , "!!Hi !!hi!!! !hi")
+    // Test.assertSimilar(remove("!!!Hi !!hi!!! !hi",3) , "Hi !!hi!!! !hi")
+    // Test.assertSimilar(remove("!!!Hi !!hi!!! !hi",5) , "Hi hi!!! !hi")
+    // Test.assertSimilar(remove("!!!Hi !!hi!!! !hi",100) , "Hi hi hi")
+    // })})
+
+    // ******MY ANSWER********
+
+    function remove(s,n){
+        return n>0 ? remove(s.replace('!', ''), n-1) : s;
+      }
