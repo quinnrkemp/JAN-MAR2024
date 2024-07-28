c93aa1805a4efa5a7e52899c81585be1a9306f1f
quinnrkemp <a467814@gmail.com>
2024-07-27 23:37:37 -0400

    coding chall for 072724

---
+++ Desktop/WEB DEVELOPER-CODING/GITHUB RELATED FOLDER/CODEWARS CHALLENGES/JAN-MAR2024/072724.js
@@ -0,0 +1,17 @@
+// (3rd day of "Objects" practice)
+
+class Person {
+    constructor (firstName='John',lastName='Doe',age=0,gender='Male') {
+        this.firstName=firstName
+        this.lastName=lastName
+        this.age=age
+        this.gender=gender     
+    }           
+    sayFullName() {
+        return `${this.firstName} ${this.lastName}`
+    }
+    static greetExtraTerrestrials(raceName) {
+        return `Welcome to Planet Earth ${raceName}`
+    }
+    // Get coding in ES6 :D
+          }
