80ca892010b635aea175f86d02ca6f34ba870b88
quinnrkemp <a467814@gmail.com>
2024-08-15 23:55:45 -0400

    coding chall for 081524

---
+++ Desktop/WEB DEVELOPER-CODING/GITHUB RELATED FOLDER/CODEWARS CHALLENGES/JAN-MAR2024/081524.js
@@ -0,0 +1,56 @@
+ // CHALLENGE NAME-  Stringy Strings
+
+    // DESCRIPTION:
+
+    // write me a function stringy that takes a size and 
+    // returns a string of alternating 1s and 0s.
+
+    // the string should start with a 1.
+    
+    // a string with size 6 should return :'101010'.
+    
+    // with size 4 should return : '1010'.
+    
+    // with size 12 should return : '101010101010'.
+    
+    // The size will always be positive and will only use whole numbers.
+
+    // // ***STARTER CODE***
+
+    // function stringy(size) {
+    //     // your code here   
+    //   } 
+
+    // //   ******TEST CASES*****
+    
+    // const Test = require('@codewars/test-compat');
+
+    // describe("the stringy function", function() {
+    
+    //   it('Should return a string', function() {
+    //     Test.assertEquals(typeof stringy(3), 'string',
+    //     'Make sure it\'s a string not a number (the function is called stringy after all!)');
+    //   });
+    
+    //   it('Should start with a 1', function() {
+    //     Test.assertEquals(stringy(3)[0], '1',
+    //     'Whoops your string doesnt start with a 1?');
+    //   });  
+    
+    //   it('Should have the correct length', function() {  
+    //     for(let i = 0; i < 10; ++i){
+    //       let parameter = (Math.random() * 50 + 1) | 0;
+    //       Test.assertEquals(stringy(parameter).length, parameter, 'Wrong length on ' + parameter);
+    //     }
+    //   });
+    // });
+
+    // ******MY ANSWER********
+
+    function stringy(size) {
+        var str = ''
+        for ( var now = 1; now <= size; ++now ) str += now % 2;
+        return str;
+      }
+
+    //   ***this is not my answer. I could not answer this one***
