ad2e69ac9171ff18f3ea2829ac9b9f6190d33fd4
quinnrkemp <a467814@gmail.com>
2024-09-01 17:13:28 -0400

    coding chall for 090124

---
+++ Desktop/WEB DEVELOPER-CODING/GITHUB RELATED FOLDER/CODEWARS CHALLENGES/JAN-MAR2024/090124.js
@@ -0,0 +1,24 @@
+ // CHALLENGE NAME-  no coding challenge. just practicing basic coding with loops
+
+    // DESCRIPTION:
+
+
+
+    // ***STARTER CODE***
+
+    
+
+    //   ******TEST CASES*****
+    
+
+
+    // ******MY ANSWER********
+
+    function twoOne() {
+         
+        for (let i= 1; i<=21; i++){
+            console.log (21)
+        }
+
+    }
+    twoOne()
