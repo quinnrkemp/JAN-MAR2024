6d2fc1a5e43ca624332ee32720234e2db3529bbe
quinnrkemp <a467814@gmail.com>
2024-09-03 23:12:47 -0400

    coding chall for 090324

---
+++ Desktop/WEB DEVELOPER-CODING/GITHUB RELATED FOLDER/CODEWARS CHALLENGES/JAN-MAR2024/090324.js
@@ -0,0 +1,21 @@
+ // CHALLENGE NAME-  no codewars...just general coding practice
+
+    // DESCRIPTION:
+
+
+
+    // ***STARTER CODE***
+
+    
+
+    //   ******TEST CASES*****
+    
+
+
+    // ******MY ANSWER********
+
+    function firstLast(arr) {
+      return arr[0]<arr[arr.length-1]? 'Hi': arr[0]>arr[arr.length-1]? ('Bye'): "We close in an hour"
+        // if (arr[0])
+    }
+console.log(firstLast([5,4,3,2,5]))
