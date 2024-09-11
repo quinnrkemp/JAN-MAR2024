bf5843ebce5fe3f895527f24e8cdc80985473ebd
quinnrkemp <a467814@gmail.com>
2024-09-10 22:48:08 -0400

    coding chall for 091024

---
+++ Desktop/WEB DEVELOPER-CODING/GITHUB RELATED FOLDER/CODEWARS CHALLENGES/JAN-MAR2024/091024.js
@@ -0,0 +1,39 @@
+ // CHALLENGE NAME-  Convert a string to an array
+
+    // DESCRIPTION:
+
+    Write a function to split a string and convert it into 
+    an array of words.
+
+    Examples (Input ==> Output):
+
+    "Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", 
+    "they", "are", "my", "favorite"]
+
+    // ***STARTER CODE***
+
+    function stringToArray(string){
+
+        // code code code
+    // for(let i=0; i < string.length; i++){
+    //   console.log(string)
+    // }
+      
+    }  
+
+    //   ******TEST CASES*****
+    
+
+
+    // ******MY ANSWER********
+
+    function stringToArray(string){
+        return string.split(' ')
+        // code code code
+    // for(let i=0; i < string.length; i++){
+    //   console.log(string)
+    // }
+      
+    }
