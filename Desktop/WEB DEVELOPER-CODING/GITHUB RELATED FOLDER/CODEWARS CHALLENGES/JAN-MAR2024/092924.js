b40ec894f8a41f04941fd8e060ce45d037e10774
quinnrkemp <a467814@gmail.com>
2024-09-29 23:40:52 -0400

    coding chall for 092924

---
+++ Desktop/WEB DEVELOPER-CODING/GITHUB RELATED FOLDER/CODEWARS CHALLENGES/JAN-MAR2024/092924.js
@@ -0,0 +1,24 @@
+ // CHALLENGE NAME-    ****PRACTICE WORK ONLY****
+
+    // DESCRIPTION:
+
+
+
+    // ***STARTER CODE***
+
+    
+
+    //   ******TEST CASES*****
+    
+
+
+    // ******MY ANSWER********
+
+    let bestRappers2020= ['num','dylan','polo g', 'dylan']
+    let bestRapperAllTime= bestRappers2020.map(x=>x +x)
+    bestRapperAllTime.unshift('1','nu dylan')
+    bestRapperAllTime.push('nu2 dylan')
+    console.log(bestRapperAllTime)
+
+
+    
