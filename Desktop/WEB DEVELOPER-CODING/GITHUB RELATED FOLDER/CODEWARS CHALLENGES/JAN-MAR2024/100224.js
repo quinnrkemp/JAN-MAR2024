f8e09f07b0147dc01e37b61c66d4b8efd0339365
quinnrkemp <a467814@gmail.com>
2024-10-02 22:59:19 -0400

    coding chall for 100224

---
+++ Desktop/WEB DEVELOPER-CODING/GITHUB RELATED FOLDER/CODEWARS CHALLENGES/JAN-MAR2024/100224.js
@@ -0,0 +1,46 @@
+ // CHALLENGE NAME-  Printing Array elements with Comma delimiters
+
+
+    // DESCRIPTION:
+
+    // Input: Array of elements
+
+    // ["h","o","l","a"]
+    
+    // Output: String with comma delimited elements 
+    // of the array in th same order.
+    
+    // "h,o,l,a"
+    
+    // Note: if this seems too simple for you try 
+    // the next level
+    
+    // Note2: the input data can be: boolean array, 
+    // array of objects, array of string arrays, array 
+    // of number arrays... 😕
+
+    // // ***STARTER CODE***
+
+    // function printArray(array){
+    //     //show me the code!
+    //   }
+
+    // //   ******TEST CASES*****
+    
+    // const Test = require('@codewars/test-compat');
+
+    // describe("Tests", () => {
+    //   it("test", () => {
+    // var data = [2,4,5,2];
+    // Test.expect( printArray( data ) ==
+    //   "2,4,5,2", "int test failed" )
+    //   });
+    // });
+
+    // ******MY ANSWER********
+
+    function printArray(arr){
+        return arr.toString()
+          //show me the code!
+        }
+        
