e1a5a55056201b7cc43425cc249944fd38cb6f02
quinnrkemp <a467814@gmail.com>
2024-10-23 23:27:00 -0400

    coding chall for 102324

---
+++ Desktop/WEB DEVELOPER-CODING/GITHUB RELATED FOLDER/CODEWARS CHALLENGES/JAN-MAR2024/102324.js
@@ -0,0 +1,18 @@
+// ******practice coding***********
+
+    class MakeGuy {
+        constructor(guyHealth, guyHeight, guySpeed, guyWeapon) { 
+        this.health= guyHealth
+        this.height= guyHeight
+        this.speed= guySpeed
+        this.weapon= guyWeapon
+        }
+        finish() {
+            alert ('get over here!!')
+        }
+    }
+  let ChunLee= new MakeGuy ('100', '6 feet', 'fast', 'gun' )
+
+  console.log(ChunLee.health)
+
+
