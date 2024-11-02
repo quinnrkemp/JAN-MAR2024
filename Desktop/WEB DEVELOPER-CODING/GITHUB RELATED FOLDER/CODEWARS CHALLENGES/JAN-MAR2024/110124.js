8daa8ad015578bc335d05f9ef0ebd4f689dfd52a
quinnrkemp <a467814@gmail.com>
2024-11-01 23:57:15 -0400

    coding chall for 110124

---
+++ Desktop/WEB DEVELOPER-CODING/GITHUB RELATED FOLDER/CODEWARS CHALLENGES/JAN-MAR2024/110124.js
@@ -0,0 +1,13 @@
+// practice work (yesterday's codewars)
+    
+    var countSheep = function (num){
+        let str= ''
+        for (let i=1;i<=num;i++){
+       //     str+=`${i} sheep...`
+          str.concat(`${i} sheep...`)
+        }
+         return str
+         //your code here
+       }
+
+    //    ********answer is incorrect********
