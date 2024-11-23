52de63fd426befcad7389818a1c202815c979f20
quinnrkemp <a467814@gmail.com>
2024-11-22 23:14:54 -0500

    coding chall for 112224

---
+++ Desktop/WEB DEVELOPER-CODING/GITHUB RELATED FOLDER/CODEWARS CHALLENGES/JAN-MAR2024/112224.js
@@ -0,0 +1,24 @@
+    // CHALLENGE NAME-  ****no codewars today***just practice coding***
+
+    // DESCRIPTION:
+
+
+
+    // ***STARTER CODE***
+
+    
+
+    //   ******TEST CASES*****
+    
+
+
+    // ******MY ANSWER********
+function numOne(num){
+    let power= ['mighty', 'alien', 'zeo']
+    for (let i=power[0]; i<=power.length-1; i++) { 
+        console.log (i)
+}
+numOne()
+}
+
+// ****code above is incomplete****
