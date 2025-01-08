77500cf68628d730a354f99fc2dd6ecffc3ecc03
quinnrkemp <a467814@gmail.com>
2025-01-07 21:43:28 -0500

    coding chall for 010725

---
+++ Desktop/WEB DEVELOPER-CODING/GITHUB RELATED FOLDER/CODEWARS CHALLENGES/JAN-MAR2024/010725.js
@@ -0,0 +1,46 @@
+    // CHALLENGE NAME-  Are you Playing Banjo?
+
+    // DESCRIPTION:
+
+    // Create a function which answers the question "Are you 
+    // playing banjo?".
+    // If your name starts with the letter "R" or lower case "r", 
+    // you are playing banjo!
+    
+    // The function takes a name as its only argument, and returns 
+    // one of the following strings:
+    
+    // name + " plays banjo" 
+    // name + " does not play banjo"
+    // Names given are always valid strings.
+
+    // // ***STARTER CODE***
+
+    // function areYouPlayingBanjo(name) {
+    //     // Implement me
+    //     return name;
+    //   }  
+
+    // //   ******TEST CASES*****
+    
+    // const chai = require("chai");
+    // const assert = chai.assert;
+    // chai.config.truncateThreshold=0;
+    
+    // describe("Basic tests", () => {
+    //   it("Testing for fixed tests", () => {
+    //     assert.strictEqual(areYouPlayingBanjo("Adam"), "Adam does not play banjo");
+    //     assert.strictEqual(areYouPlayingBanjo("Paul"), "Paul does not play banjo");
+    //     assert.strictEqual(areYouPlayingBanjo("Ringo"), "Ringo plays banjo");
+    //     assert.strictEqual(areYouPlayingBanjo("bravo"), "bravo does not play banjo");
+    //     assert.strictEqual(areYouPlayingBanjo("rolf"), "rolf plays banjo");
+    //     })
+    //   })
+
+    // ******MY ANSWER********
+
+    function areYouPlayingBanjo(name) {
+        return name[0].toLowerCase()==='r'?`${name} plays banjo`:`${name} does not play banjo`
+          // Implement me
+          return name;
+        }
