cdc7185b905ea429325ea23c8a17b86eb895df03
quinnrkemp <a467814@gmail.com>
2025-01-17 23:32:49 -0500

    coding chall for 011725

---
+++ Desktop/WEB DEVELOPER-CODING/GITHUB RELATED FOLDER/CODEWARS CHALLENGES/JAN-MAR2024/011725.js
@@ -0,0 +1,65 @@
+    // CHALLENGE NAME-  Cat years, Dog years
+
+    // DESCRIPTION:
+
+    // I have a cat and a dog.
+
+    // I got them at the same time as kitten/puppy. That was humanYears years ago.
+    
+    // Return their respective ages now as [humanYears,catYears,dogYears]
+    
+    // NOTES:
+    
+    // humanYears >= 1
+    // humanYears are whole numbers only
+    // Cat Years
+    // 15 cat years for first year
+    // +9 cat years for second year
+    // +4 cat years for each year after that
+    // Dog Years
+    // 15 dog years for first year
+    // +9 dog years for second year
+    // +5 dog years for each year after that
+ 
+    // // ***STARTER CODE***
+
+    // var humanYearsCatYearsDogYears = function(humanYears) {
+    //     // Your code here!
+    //     return [0,0,0];
+    //   }    
+
+    // //   ******TEST CASES*****
+    
+    // const Test = require('@codewars/test-compat');
+
+    // describe("Example Tests", function() {
+    
+    //   it("one", function() {
+    //     Test.assertSimilar(humanYearsCatYearsDogYears(1), [1,15,15]);
+    //   });
+    
+    //   it("two", function() {
+    //     Test.assertSimilar(humanYearsCatYearsDogYears(2), [2,24,24]);
+    //   });
+    
+    //   it("ten", function() {
+    //     Test.assertSimilar(humanYearsCatYearsDogYears(10), [10,56,64]);
+    //   });
+    
+    // });
+
+    // ******MY ANSWER********
+
+    var humanYearsCatYearsDogYears = function(hum) {
+        let alt=hum-2
+      
+        if (hum<2){
+         return [1,15,15]
+       } 
+        else if (hum<3){
+         return [2,24,24]
+          }
+        return [hum,(alt*4)+24,(alt*5)+24];
+      }
+
+     
